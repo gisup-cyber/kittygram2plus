2d7ac0c911f6cdad8cc12b87813d445fdc965bc7
Игорь Васильев <assists1@yandex.ru>
2023-02-04 15:47:05 +0300

    Add permission_classes at viewset

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
